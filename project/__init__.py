913c95333864c58bda9b13d3f4365ff6da244f55
dowidev <doni.wi@outlook.com>
2019-04-28 14:49:33 +0700

    Initial flash

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,19 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    categories = requests.get('http://127.0.0.1:5001/categories')
+    categories = categories.json()
+
+    news = requests.get('http://127.0.0.1:5001/news')
+    news = news.json()
+    return render_template('index.html', categories=categories, news=news)
+
+
+
+@app.route('/hello')
 def hello_world():
     return render_template('index.html')
